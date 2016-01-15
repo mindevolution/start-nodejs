ac7b0f68214bb592944cccbb56ee14346026096a
Zhili He <zhili850702@gmail.com>
2016-01-15 17:25:16 +0800

    Create the echo server

---
+++ echo-server.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+http.createServer(function (request, response) {
+	if (request.method === 'GET' && request.url === '/echo') {
+		var body = [];
+		request.on('data', function (chunk) {
+			body.push(chunk);
+		}).on('end', function () {
+			body = Buffer.concat(body).toString();
+			response.end(body);
+		});
+	} else {
+		response.statusCode = 404;
+		response.end();
+	}
+}).listen(8001);
